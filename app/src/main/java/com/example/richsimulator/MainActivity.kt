6a9e193bf0e8d55908440a1c7afa95855df13d31
timkuo25 <b05703100@ntu.edu.tw>
2020-09-02 21:27:45 +0800

    Register Page

---
+++ app/src/main/java/com/example/richsimulator/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.richsimulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickRegister(view: View){
+        registerPageMessage.text = "User ID = " + userIdRegister.text + "\n" +
+                                   "Password = " + passwordRegister.text + "\n" +
+                                   "Email = " + emailRegister.text + ""
+    }
+}
